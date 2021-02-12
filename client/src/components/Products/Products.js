971141dea51c227969733992491eace37cd4dc40
emersonpessoa01 <eps011108@gmail.com>
2021-02-12 14:13:40 -0300

    added Form

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,12 +1,44 @@
 import React from "react";
 import "../../components/Promotion/Card/Card.css"
-import Product from "../Products/Product"
+// import Product from "../Products/Product"
 
 function Products({ products }) {
   return (
     <div>
-      {products.map((product) => {
-        return <Product key={product.product} product={product} />
+      {products.map(({imageUrl, title, price, comments, url}) => {
+        return (
+          <div className="promotion-card" key={imageUrl} >
+            <img src={imageUrl} className="promotion-card__image" alt={imageUrl} />
+            <div className="promotion-card__info">
+              <h1 className="promotion-card__title">{title}</h1>
+              <span className="promotion-card__price">
+                {price.toLocaleString("pt-br", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </span>
+              <footer className="promotion-card__footer">
+                {comments.length > 0 && (
+                  <div className="promotion-card__comment">
+                    "{comments[0].comment}"
+                  </div>
+                )}
+                <div className="promotion-card__comments-count">
+                  {comments.length}{" "}
+                  {comments.length > 1 ? "Comentários" : "Comentário"}
+                </div>
+                <a
+                  href={url}
+                  target="_blank"
+                  className="promotion-card__link"
+                  rel="noopener noreferrer"
+                >
+                  IR PARA O SITE
+                </a>
+              </footer>
+            </div>
+          </div>
+        );
       })}
     </div>
   );
